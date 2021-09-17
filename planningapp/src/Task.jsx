23fa99604708653f1248c0304b4058d53ec7d6ee
Justin Holsclaw <Justin.Holsclaw@snow.edu>
2021-09-17 13:27:32 -0600

    needed to pull

--- planningapp/src/Task.jsx
+++ planningapp/src/Task.jsx
@@ -1,8 +1,21 @@
+import React from "react";
+import ReactDOM from 'react-dom';
 import { useCallback, useState } from "react";
 import EditForm from "./EditForm";
 
+/*const updateTaskForm = (props) =>
+    
+ {return(
+    <React.Fragment>
+        ReactDOM.createPortal(
+            <EditForm onClick={props.updateTask}/>,
+        document.getElementById('backdrop-root')
+                            )   
+    </React.Fragment>
+)
+}*/
+
 function Task(props){
-    const [isEditing, setIsEditing] = useState(false)
     return(
         <>
             <div className="row border border-secondary">
@@ -12,10 +25,8 @@ function Task(props){
                     <h3>{props.time}</h3>
                 </div>
                 <div className="col">
-                {isEditing
-                    && <EditForm   post={props.post}></EditForm>
-                }
-                {!isEditing && <button type="button" id={props.title} onClick={props.updateTask} className="btn btn-warning btn-lg">✏</button>}
+               
+                <button type="button" id={props.title} /*onClick={updateTaskForm}*/  className="btn btn-warning btn-lg">✏</button>
                 </div>
                 <button className="col btn btn-danger" id={props.title} onClick={props.deleteTask}>❌</button>
             </div>
